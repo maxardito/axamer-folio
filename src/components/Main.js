0f2d1233d11ec2086179a46b9542ae970f2aaa4a
Max Ardito <maxwellardito@gmail.com>
2020-07-25 15:04:25 -0400

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "./main.module.scss";
+
+const Main = () => {
+  return <div className={Style.title}>axamer folio eric wubbels</div>;
+};
+
+export default Main;
