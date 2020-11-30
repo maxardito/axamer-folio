56c5d5bc34315800aeb196556a7f32c22781e612
Max Ardito <maxwellardito@gmail.com>
2020-11-30 11:02:42 -0500

    Town coordinate functions

--- src/components/Journey/Journey.js
+++ src/components/Journey/Journey.js
@@ -14,7 +14,7 @@ import Movements from "../Movements.json";
  * 
  * @param {*} sequence The sequence 
  */
-const Journey = ({ sequence, strokeColor, fillColor, visible, selectedTown, nextTown, previousTown, isConvolved, polygonOpacity }) => {
+const Journey = ({ sequence, strokeColor, fillColor, visible, selectedTown, nextTown, previousTown, polygonOpacity }) => {
 
     const polygonOption = {
         strokeColor: strokeColor,
@@ -46,7 +46,7 @@ const Journey = ({ sequence, strokeColor, fillColor, visible, selectedTown, next
         strokeWeight: 6,
     };
 
-    const convolvedTownPolyline = {
+    const asyncTownPolyline = {
         geodesic: true,
         strokeColor: "purple",
         visible: visible,
@@ -54,33 +54,77 @@ const Journey = ({ sequence, strokeColor, fillColor, visible, selectedTown, next
         strokeWeight: 6,
     };
 
+    function getSelectedTownCoordinates(backwards) {
+        let direction = backwards ? 0 : 1
+        if (Array.isArray(selectedTown)) {
+            return { lat: selectedTown[direction].lat, lng: selectedTown[direction].lng }
+        } else {
+            return { lat: selectedTown.lat, lng: selectedTown.lng }
+        }
+    }
+
+    function getNextTownCoordinates() {
+        if (Array.isArray(nextTown)) {
+            return { lat: nextTown[0].lat, lng: nextTown[0].lng }
+        } else {
+            return { lat: nextTown.lat, lng: nextTown.lng }
+        }
+    }
+
+    function getPreviousTownCoordinates() {
+        if (Array.isArray(previousTown)) {
+            return { lat: previousTown[1].lat, lng: previousTown[1].lng }
+        } else {
+            return { lat: previousTown.lat, lng: previousTown.lng }
+        }
+    }
+
     return (
         <>
             <Polygon
                 paths={sequence.map((num, key) => {
-                    return {
-                        lat: Movements.metadata[num].lat,
-                        lng: Movements.metadata[num].lng
+                    if (Array.isArray(num)) {
+                        return ({
+                            lat: Movements.metadata[num[0]].lat,
+                            lng: Movements.metadata[num[0]].lng
+                        },
+                        {
+                            lat: Movements.metadata[num[1]].lat,
+                            lng: Movements.metadata[num[1]].lng
+                        })
+                    } else {
+                        return {
+                            lat: Movements.metadata[num].lat,
+                            lng: Movements.metadata[num].lng
+                        }
                     }
                 })}
                 options={polygonOption}
             />
             <Polyline
                 path={[
-                    { lat: selectedTown.lat, lng: selectedTown.lng },
-                    { lat: nextTown.lat, lng: nextTown.lng }]
+                    getSelectedTownCoordinates(false),
+                    getNextTownCoordinates()]
                 }
                 options={nextTownPolyline}
 
             />
             <Polyline
                 path={[
-                    { lat: selectedTown.lat, lng: selectedTown.lng },
-                    { lat: previousTown.lat, lng: previousTown.lng }]
+                    getSelectedTownCoordinates(true),
+                    getPreviousTownCoordinates()]
                 }
-                options={isConvolved ? convolvedTownPolyline : previousTownPolyline}
+                options={previousTownPolyline}
 
             />
+            {Array.isArray(selectedTown) ? <Polyline
+                path={[
+                    { lat: selectedTown[0].lat, lng: selectedTown[0].lng },
+                    { lat: selectedTown[1].lat, lng: selectedTown[1].lng }]
+                }
+                options={asyncTownPolyline}
+
+            /> : null}
         </>
     )
 }
