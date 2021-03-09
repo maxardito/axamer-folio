26216d866256f3669c490a4567aba3e47033c6ea
Max Ardito <maxwellardito@gmail.com>
2021-03-09 16:59:22 -0500

    New program notes

--- src/components/ProgramNotes.js
+++ src/components/ProgramNotes.js
@@ -1,57 +1,107 @@
 const ProgramNotes = `
 # Axamer Folio [2015]
-**by Eric Wubbels**
-
 _for saxophone and drumset_
 
-Axamer Folio is a modular network of 24 pieces for saxophone and drumset, with no pre-set order,
-form, or duration.
-The individual pieces (which include duos, solos, and duos that can be separated and combined with
-other pieces) project a small number of musical objects in an extremely diverse range of performative
-and notational contexts, from rigorously specified to indeterminate, graphic, and text scores; from
-tightly synchronized duo music to phasing loops, list structures, and free improvisation. The result is a
-kind of self-similar labyrinth of possibilities, within which the performers trace a path.
-Axamer Folio is dedicated to Anthony Braxton and mathias spahlinger.
+_Axamer Folio_ is a modular network of 24 pieces for saxophone and drumset, with no pre-set
+order, form, or duration.
 
-**For Performers: Constructing a Version**
+The individual pieces (which include duos, solos, and duos that can be separated and
+combined with other pieces) project a small number of musical objects in an extremely
+diverse range of performative and notational contexts, from rigorously specified to
+indeterminate, graphic, and text scores; from tightly synchronized duo music to phasing
+loops, list structures, and free improvisation. The result is a kind of self-similar labyrinth of
+possibilities, within which the performers trace a path.
 
-First, decide on the general overall duration: short [~7-9'], standard [~10-19'], or long [~20-25+'].
+* * * * * * * * *
 
-Then, start by making an initial selection of some of the modules you want in the version. No version can or will include all modules; it's up to the performers to choose what to include, and what to emphasize.
+On a visit to Innsbruck in the summer of 2014 I was struck by the wonderfully weird, weirdly
+consistent names of many of the small Alpine towns in the Süd-Tirol region. The names
+follow several patterns, as if devised in accordance with some arcane, medieval formula:
 
-Third, begin to “build out” the form of the version, referring to the “routing” diagrams for each module. Each piece has a specific of set of modules that can precede it, and a set that it can move on to, so begin arranging your chosen modules according to these pathways.
+- almost always one or maybe two syllables (if two, always with first syllable stress);
+- often so stuffed with consonants that vowels are barely able to squeeze themselves in (_Gfass_,
+_Patsch_, _Igls_, _Arzl_, _Pfosch_);
+- often occurring in matched pairs across the region (_Völs/Lans_, _Dafl/Dörfl_, _Tulfes/Telfes_,
+_Mutters/Natters_)
 
-Choose additional modules to connect these, as necessary, keeping in mind that modules that are “separable duos,” or ones can be played as individual solos, can overlap. In this case:
+Taking up this modular project the following year, it felt right – though I couldn't explain
+exactly why – that each module should be named (in the way you'd name a pet, or a child),
+rather than titled. These names were all there waiting for me, and attached themselves
+quickly to the music.
 
-> – if both of the overlapping modules are “frame compatible,” a common meter and pulse can bemaintained across the duo, if desired. (For example, the saxophonist could begin INZIG after two pages of LANS, and the the two pieces would proceed together in the same tempo and
-metrical structure.)
+One consequence of the choice of actual place names for the titles of the modules is that it
+allows for the visualization of any given version as a literal “mapping” of the form, overlaid
+on the physical terrain itself. As a result, a move from one module to another in the
+conceptual space of the piece maps directly to a move from one town to another in real,
+physical space, and a version as a whole when traced draws a criss-crossing route across the
+southern Alps.
 
-> – Or, the players can enter a period of asynchronization, and re-orient at a later point by means of a pre-arranged cue
+Many thanks to Max Ardito for his work in coding and designing the visualization interface
+for this aspect of the piece.
 
-Depending on the players' preference, a version may be almost entirely synchronized and precisely co-ordinated duo music, or there may be substantial periods of independence, free polyphony, improvisation, and solo playing.
+# For Performers: Constructing a Version
 
-A few more specific constraints...
+First, decide on the general overall duration: short [~7-9'], standard [~10-19'], or long [~20-
+25+'].
+
+Then, start by making an initial selection of some of the modules you want in the version. No
+version can or will include all modules; it's up to the performers to choose what to include,
+and what to emphasize.
+
+Third, begin to “build out” the form of the version, referring to the “routing” diagrams for
+each module. Each piece has a specific of set of modules that can precede it, and a set that it
+can move on to, so begin arranging your chosen modules according to these pathways.
+
+Choose additional modules to connect these, as necessary, keeping in mind that modules that
+are “separable duos,” or ones can be played as individual solos, can overlap. In this case:
+
+- if both of the overlapping modules are “frame compatible,” a common meter and
+pulse can be maintained across the duo, if desired. (For example, the saxophonist
+could begin INZIG after two pages of LANS, and the the two pieces would proceed
+together in the same tempo and metrical structure.)
+– Or, the players can enter a period of asynchronization, and re-orient at a later point by
+means of a pre-arranged cue
+
+Depending on the players' preference, a version may be almost entirely synchronized and
+precisely co-ordinated duo music, or there may be substantial periods of independence, free
+polyphony, improvisation, and solo playing.
+
+* * * * * * * * *
+
+A few more specific constraints:
 
 ## 1. Beginning and Ending
+
 Any module or pair of modules can begin or end the piece, with the following exceptions:
 
-– ARZL/THAUR/RINN, BIRGITZ, MIEDERS, and NEDER are non-initial, and can't begin a version;
-
-– ARZL/THAUR/RINN, AXAMER-LIZUM, and KREITH/GRIES AM BRENNER are non-final, and can't end a version.
+- ARZL/THAUR/RINN, BIRGITZ, MIEDERS, and NEDER are non-initial, and can't begin a version;
+- ARZL/THAUR/RINN, AXAMER-LIZUM, and KREITH/GRIES AM BRENNER are non-final, and can't end a version.
 
 ## 2. MEDRAZ
-There is no module that all versions must include, but MEDRAZ is the most likely to occur, as it's connected to almost every other, the “central hub” of the routing network. By default MEDRAZ should be in any version; its exclusion should be a conscious, reasoned choice. When included, MEDRAZ is played either twice or three times, each with a separate tempo and “protocol” (as outlined on its title page), either distributed through the form, or played consecutively.
+
+There is no module that all versions must include, but MEDRAZ is the most likely to occur, as
+it's connected to almost every other, the “central hub” of the routing network. By default
+MEDRAZ should be in any version; its exclusion should be a conscious, reasoned choice.
+When included, MEDRAZ is played either twice or three times, each with a separate tempo
+and “protocol” (as outlined on its title page), either distributed through the form, or played
+consecutively.
 
 ## 3. “Loop Circuit” Modules (ARZL, THAUR, RINN)
-Any version over 10' long should include one of the “loop circuit” modules (ARZL, THAUR, or
-RINN), though no version can contain more than one.
+
+Any version over 10' long should include one of the “loop circuit” modules (ARZL, THAUR,
+or RINN), though no version can contain more than one.
 
 ## 4. Repetition of Modules Within a Form
-No module can be repeated within a version, except AXAMS, which can be played once or twice,
-separated by other pieces. Modules that can be played as solos for either instrument (MIEDERS, NEDER) can be played twice if played by different players on each occasion.
+
+No module can be repeated within a version, except AXAMS, which can be played once or
+twice, separated by other pieces. Modules that can be played as solos for either instrument
+
+(MIEDERS, NEDER) can be played twice if played by different players on each occasion.
 
 ## 5. Improvised/Fixed Modules
-In the case of the modules with paired improvised versions (KREITH / GRIES AM BRENNER, GÖTZENS / PRAXMAR), only one or the other should be included in a version.
+
+In the case of the modules with paired improvised versions (KREITH / GRIES AM
+BRENNER, GÖTZENS / PRAXMAR), only one or the other should be included in a version.
 `
 
 export default ProgramNotes
